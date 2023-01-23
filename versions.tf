5a7dec1ad506713211c801e52f77579702de7965
cklewar <cklewar@t-online.de>
2023-01-23 18:11:20 +0100

    0.11.18

--- versions.tf
+++ versions.tf
@@ -8,14 +8,13 @@ terraform {
       name = "f5-xc-site-mesh-group-module"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.14"
+      source = "volterraedge/volterra"
+      version = "= 0.11.18"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
